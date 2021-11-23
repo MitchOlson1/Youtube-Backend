db9db9ac90429312e6d3250f802a1061e46c3a6b
unknown <nolan.beihl@gmail.com>
2021-11-23 09:51:09 -0700

    Working on setting up API

--- youtubeclone/local_settings.py
+++ youtubeclone/local_settings.py
@@ -2,7 +2,13 @@ SECRET_KEY = 'django-insecure-q#^3irn3k(3=rkirg5ynnmy%1(kq2ue_sr9bsn8w&5btp=-50h
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'mysql.connector.django',
+        'NAME' : 'youtubeclone',
+        'USER' : 'root',
+        'PASSWORD' : 'MyPassword3!',
+        'HOST' : '127.0.0.1',
+        'OPTIONS' : {
+            'autocommit' : True
+        }
     }
 }
